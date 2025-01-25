cf773f5f854d66ad38f8462c01797775c4810536
Ian-PB <C00294680@setu.ie>
2025-01-25 00:29:24 +0000

    Made underwater shader

--- include/GamePlay.h
+++ include/GamePlay.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 class GamePlay
 {
@@ -7,11 +8,22 @@ public:
 	GamePlay();
 
 	void update(double t_deltaTime);
-	void render(sf::RenderWindow& t_window) const;
+	void render(sf::RenderWindow& t_window);
 	void processEvents(sf::Event& t_event);
 	void processKeys(sf::Event& t_event);
 
 private:
+
+	// Shader
+	sf::Clock clock;
+	sf::Texture noiseTexture;
+	void loadShader();
+	sf::Shader underWaterShader;
+	sf::RenderTexture renderTexture;
+
+	// TEMP TEXTURE
+	sf::Texture tiles;
+	sf::Sprite tileSprite;
 
 	sf::CircleShape circle;
 
--- src/GamePlay.cpp
+++ src/GamePlay.cpp
@@ -1,18 +1,36 @@
 #include "GamePlay.h"
+#include "Globals.h"
 
 GamePlay::GamePlay()
 {
+	loadShader();
+
 	circle.setRadius(50);
-	circle.setFillColor(sf::Color::Blue);
+	circle.setFillColor(sf::Color::Green);
+	circle.setPosition(100, 100);
+
+	if (!renderTexture.create(SCREEN_WIDTH, SCREEN_HEIGHT))
+	{	
+		std::cout << "Couldnt create texture\n";
+	}
 }
 
 void GamePlay::update(double t_deltaTime)
 {
+	underWaterShader.setUniform("time", clock.getElapsedTime().asSeconds());
 }
 
-void GamePlay::render(sf::RenderWindow& t_window) const
+void GamePlay::render(sf::RenderWindow& t_window) 
 {
-	t_window.draw(circle);
+	renderTexture.clear();
+
+	renderTexture.draw(tileSprite);
+	renderTexture.draw(circle);
+
+	renderTexture.display();
+
+	sf::Sprite screenSprite(renderTexture.getTexture());
+	t_window.draw(screenSprite, &underWaterShader);
 }
 
 void GamePlay::processEvents(sf::Event& t_event)
@@ -21,4 +39,29 @@ void GamePlay::processEvents(sf::Event& t_event)
 
 void GamePlay::processKeys(sf::Event& t_event)
 {
+}
+
+void GamePlay::loadShader() // shader.setUniform("texture", sf::Shader::CurrentTexture);
+{
+	if (!underWaterShader.loadFromFile("Assets/Shaders/UnderWater.frag", sf::Shader::Fragment))
+	{
+		std::cout << "COULDN'T LOAD SHADER \n";
+	}
+	if (!noiseTexture.loadFromFile("Assets/Shaders/Perlin.png"))
+	{
+		std::cout << "Couldnt load noise texture\n";
+	}
+	noiseTexture.setRepeated(true);
+	noiseTexture.setSmooth(true);
+	underWaterShader.setUniform("noiseTexture", noiseTexture);
+
+
+	if (!tiles.loadFromFile("Assets/Shaders/tiles.jpg"))
+	{
+		std::cout << "Couldnt load tiles texture\n";
+	}
+	tiles.setRepeated(true);
+	tileSprite.setTexture(tiles);
+	tileSprite.setTextureRect(sf::IntRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+
 }
